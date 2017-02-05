a832ad2b14ae8bac55ed54a152f210936ea4f628
Sean Byfield <seanbyfield@Seans-MacBook-Air.local>
2017-02-05 04:58:52 -0500

    master2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,90 @@
       var wins = 0;
       var loses = 0;
       var guessLeft = 9;
-      var randLetter;
-      var userGuess;
+      //var guesses = 9;
+      //var randLetter;
+      var guessedLetters = [];
+      var computerGuess;
 
-      var targetDiv = document.getElementById("totalGuess");
+      var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-            targetDiv.innerHTML = guessLeft;
-      
-      var psychicGame = {
+      // get a random letter from the LetterArray 
+      var randomGuess = Math.floor(Math.random() * letterArray.length);
 
-      getUserInput: function()
+      updateGuesses = function()
       {
-        var userInput = document.getElementById("userInput").value;
-        console.log("user input " + userInput);
-        $("#lettersGuessed").append(userInput);        
+        console.log("in updateGuesses");
+        document.querySelector("#totalGuess").innerHTML = guessLeft;
+        console.log(guessLeft);
+      };
+      //updateGuesses();
 
-        psychicGame.getLetter();
-        userGuess = userInput;
-        document.getElementById("user").reset();
-      },
 
+      var getComputerGuess = function()
+      {
+        console.log("in getComputerGuess function");
+        this.computerGuess = this.letterArray[Math.floor(Math.random() * this.letterArray.length)];
+        
+
+        console.log("The computer chose: " + computerGuess);
+        
+      };
+      //getComputerGuess();
+
+      var lettersGuessed = function() 
+      {
+        console.log("in lettersGuessed function");
+        // Display the letters guessed by the user
+        document.querySelector('#lettersGuessed').innerHTML = guessedLetters.join(', ');
+      };
+
+      var resetFunction = function()
+      {
+        console.log("in resetFunction");
+      guessLeft = 9;
+      guesses = 9;
+      
+      guessedLetters = [];
+      
+      getComputerGuess();
+      updateGuesses();
+      lettersGuessed();
+      }
+      getComputerGuess();
+      updateGuesses();
+
+      document.onkeyup = function(event) 
+      {
+        console.log("in document.onkeyup function");
+        guessLeft--;
+        var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+        guessedLetters.push(userInput);
+        updateGuesses();
+        lettersGuessed();
+        console.log("userInput: " + userInput);
+        console.log("computer's: " + computerGuess);
+
+        if (guessLeft > 0){
+            if (userInput == computerGuess){
+                wins++;
+                document.querySelector("#totalWin").innerHTML = wins;
+                console.log("wins = : " + wins);
+                resetFunction();
+            }
+        }else if(guessLeft == 0){
+            // Then we will loss and we'll update the html to display the loss 
+            loses++;
+            document.querySelector("#totalLoses").innerHTML = loses;
+            console.log("loses = : " + loses);
+            // Then we'll call the reset. 
+            resetFunction();
+        }
+};
+
+
+
+/*
       getLetter: function(){
         var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -35,45 +99,8 @@
         guessLeft--;
         console.log("you have " + guessLeft + "guesses left");
         var targetDiv = document.getElementById("totalGuess");
-
-            targetDiv.innerHTML = guessLeft;
-        
-
-        if (guessLeft > 0)
-        {
-          console.log(letterArray[randno]);
-          psychicGame.compareLetter();
-        }
-        else if (guessLeft === 0){
-          wins = 0;
-          loses = 0;
-          guessLeft = 9;
-          document.getElementById("user").reset();
-          return;
-        }
-      },
-
-      compareLetter: function(){
-
-        if (userGuess == userInput){
-            win++
-            console.log("wins = " + wins);
-            var targetDiv = document.getElementById("totalWin");
-
-      targetDiv.innerHTML = wins;
-
-      //targetDiv.appendChild(wins);
-          }
-          else{
-            loses++
-            console.log("loses = " + loses);
-            var targetDiv = document.getElementById("totalLoses");
-
-            targetDiv.innerHTML = loses;
-
-            //targetDiv.appendChild(loses);
-          }
-      }
+*/
+    //  }
 
       // ...
-};
+//};
